8025e5ac5d1d0763d2d7059aa055f491feb61afd
Yan <fzerix@gmail.com>
2024-02-22 22:48:21 +0300

    change design

--- popup.tsx
+++ popup.tsx
@@ -9,16 +9,20 @@ function IndexPopup() {
 
 
   return (
-    <div className="">
-      <input value={value} onChange={(e) => setValue(e.target.value)}
-      className='px-2 bg-slate-700'
-      placeholder="word" />
-      <button onClick={async () => {
-        const url = await getWordPronunciation(value);
-        setPronunciationUrl(url);
-      }}>Get Pronunciation</button>
+    <div className="p-5">
+      <div className="flex gap-2 text-[20px] ">
+        <input value={value} onChange={(e) => setValue(e.target.value)}
+        className='px-2 text-white bg-slate-700'
+        placeholder="word" />
+        <button onClick={async () => {
+          const url = await getWordPronunciation(value);
+          setPronunciationUrl(url);
+        }}>Get</button>
 
-      {pronunciationUrl && <audio controls src={pronunciationUrl} />}
+      </div>
+
+      {pronunciationUrl && <audio className="mt-2" controls src={pronunciationUrl} />}
+
     </div>
   ) 
 }
